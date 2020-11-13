67e1d5f389eb680d0297c72bd8ee599d40cfd67e
Chongor Szilagyi <chongor002@gmail.com>
2020-11-13 08:12:11 +0000

    Initial commit

---
+++ f0005b.py
@@ -0,0 +1,6 @@
+jelszo = '123'
+tipp = input('Mi a jelszo? ')
+if tipp == jelszo:
+    print('Udvozoljukt.')
+else:
+    print('Helytelen jelszo.')
---
+++ f0005d.py
@@ -0,0 +1,6 @@
+szam = input ('Mondj egy szamot')
+szam = int(szam)
+if szam > 0:
+  print('Ez egy termesztes szam')
+else:
+  print('Ezt nem tudom nem vagyok jo matekos')
